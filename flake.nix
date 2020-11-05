e76c1b74a9d6d2f272d2d864e73df44885ca1315
Shay Bergmann <sbergmann@kreisys.co>
2020-11-04 19:06:31 -0500

    Try submodules=true

--- flake.nix
+++ flake.nix
@@ -1,16 +1,20 @@
 {
   description = "K Semantics of the Ethereum Virtual Machine (EVM)";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/76aafbf4bf4992c82da41ccefd8c6d481744524c";
-  inputs.nixpkgs.flake = false;
-  inputs.utils.url = "github:numtide/flake-utils";
+  inputs = {
+    kevm = {
+      url = "github:kframework/evm-semantics?submodules=true";
+      flake = false;
+    };
+    utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, utils, nixpkgs }:
+  outputs = { self, kevm, utils, nixpkgs }:
     let
       name = "kevm";
       systems = [ "x86_64-darwin" "x86_64-linux" ];
       overlay = final: prev: {
-        kevm.defaultPackage = final.callPackage ./package.nix {};
+        kevm.defaultPackage = final.callPackage ./package.nix { src = kevm; };
       };
 
       simpleFlake = utils.lib.simpleFlake {
--- package.nix
+++ package.nix
@@ -1,4 +1,5 @@
 { stdenv
+, src
 , fetchFromGitHub
 , pandoc
 , openjdk8
@@ -25,18 +26,11 @@
 }:
 
 stdenv.mkDerivation {
+  inherit src;
+
   name = "kevm";
 
-  src = fetchFromGitHub {
-    owner = "kframework";
-    repo = "evm-semantics";
-    rev = "cc9c659510383215e5333d7f60088a2fdf2d120d";
-    sha256 = "sha256-ZY9IQKIUmbfe7qgnr69XdHe+oVq5qXiHsv24nFc/Wlo=";
-    fetchSubmodules = true;
-    leaveDotGit = true;
-  };
-
-  patches = [ ./kevm.patch ];
+  # patches = [ ./kevm.patch ];
   buildInputs = [ pandoc openjdk8 ocaml opam maven z3 mpfr autoconf automake libtool ncurses unzip git curl rsync gcc perl which pkgconfig flex zlib python3 ];
 
   configurePhase = ''
